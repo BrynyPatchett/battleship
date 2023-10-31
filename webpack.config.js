8702a293b580d18f4efb32ba6c7c5a2efdea6899
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2023-10-31 14:43:01 +1300

    Added webpack support and change file structure to support webpack output

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'BattleshipS',
+          template:'./src/index.html',
+        }),
+      ],
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+  };
