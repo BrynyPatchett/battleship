5bcacfcdbd8928fd71fc9aee87e4fa1df6f01487
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2023-11-03 16:41:37 +1300

    player can click ship position and play round against computer

--- src/js/index.js
+++ src/js/index.js
@@ -8,65 +8,6 @@ let ships = [5, 4, 3, 3, 2];
 let selectedShipIndex = 0;
 let currentPlayer = 0;
 
-// let compWins = 0;
-// let computer2Wins = 0;
-
-// for (let i = 0; i < 100; i++) {
-//   let coordi = 0;
-//   let gameboard = new Gameboard(10);
-//   let gameboardPC = new Gameboard(10);
-
-// let shiplength = 2;
-// let coords = { x: 0, y: 0 };
-// gameboard.AddShipToGrid(shiplength, coords, 0);
-// gameboardPC.AddShipToGrid(shiplength, coords, 0);
-
-// shiplength = 3;
-// coords = { x: 6, y: 4 };
-// gameboard.AddShipToGrid(shiplength, coords, 1);
-// gameboardPC.AddShipToGrid(shiplength, coords, 1);
-
-// shiplength = 3;
-// coords = { x: 6, y: 7 };
-// gameboard.AddShipToGrid(shiplength, coords, 1);
-// gameboardPC.AddShipToGrid(shiplength, coords, 1);
-
-// shiplength = 4;
-// coords = { x: 0, y: 3 };
-// gameboard.AddShipToGrid(shiplength, coords, 0);
-// gameboardPC.AddShipToGrid(shiplength, coords, 1);
-
-// shiplength = 5;
-// coords = { x: 0, y: 9 };
-// gameboard.AddShipToGrid(shiplength, coords, 0);
-// gameboardPC.AddShipToGrid(shiplength, coords, 1);
-
-// let computer = new Computer("Computer", gameboardPC);
-// let computer2 = new Computer("Computer2", gameboard);
-
-//   while (
-//     computer.gameBoard.AllSunk() !== true &&
-//     computer2.gameBoard.AllSunk() !== true
-//   ) {
-//     computer2.MakeRandomMove();
-//     if (computer2.gameBoard.AllSunk()) {
-//        console.log("Computer2 Wins");
-//        computer2Wins++;
-//       break;
-//     }
-
-//     computer.MakeRandomMove();
-//     if (computer.gameBoard.AllSunk()) {
-//       console.log("Computer Wins");
-//       compWins++;
-//       break;
-//     }
-
-//     coordi++;
-//   }
-// }
-
-// console.log(`Computer Wins: ${compWins}, Computer2 Wins: ${computer2Wins}`);
 
 const modal = document.querySelector(".modal");
 const modalContent = document.querySelector(".modal-content");
@@ -91,11 +32,11 @@ selectedPvE.addEventListener("click", () => {
   modal.style.display = "none";
   updateBoardName(playerLeftBoard, "Computer");
   updateBoardName(playerRightBoard, "You");
-  removeHoverFromBoard(playerLeftTiles);
+  removeHoverFromTiles(playerLeftTiles);
   //list of ships, add click event, then remove after each one placed
   currentDirection = 0;
   //add hover selection UI to boatin
-  addHoverPlacementToBoard(playerRightTiles,0);
+  initaliseGame(playerRightTiles, gameboardRight, 0);
 });
 
 selectedPvP.addEventListener("click", () => {
@@ -103,44 +44,60 @@ selectedPvP.addEventListener("click", () => {
   console.log("Display player names");
   modal.style.display = "none";
   currentDirection = 0;
-  removeHoverFromBoard(playerLeftTiles);
-  addHoverPlacementToBoard(playerRightTiles,1);
+  removeHoverFromTiles(playerLeftTiles);
+  addHoverPlacementToBoard(playerRightTiles, 1);
 });
 
-function removeHoverFromBoard(tiles) {
+function removeHoverFromTiles(tiles) {
   [...tiles.children].forEach((element) => {
     element.classList.add("disabled");
   });
 }
 
-function addHoverToBoard(tiles) {
+function addHoverToTiles(tiles) {
   [...tiles.children].forEach((element) => {
     element.classList.remove("disabled");
   });
 }
+let test;
 
-
-function addHoverPlacementToBoard(tiles,isPvP) {
+function initaliseGame(tiles, gameBoard, isPvP) {
   [...tiles.children].forEach((element) => {
-    element.addEventListener("mouseover",()=>{
-      hoverPlacementPreview(element,tiles);
-    });
-    element.addEventListener("click",()=>{
+    element.addEventListener(
+      "mouseover",
+      (test = function () {
+        hoverPlacementPreview(element, tiles);
+      })
+    );
+    element.addEventListener("click", () => {
       let coords = { x: +element.dataset.col, y: +element.dataset.row };
-     if(gameboardRight.AddShipToGrid(ships[selectedShipIndex],coords,currentDirection)){
-      currentLocations.forEach((element) => {
-        element.classList.add("occupied");
-      });
-      selectedShipIndex++;
-      if(selectedShipIndex === ships.length){
-        isPvP ? console.log("PvP Move onto next player, show blank screen,tear down events and add to other grid") : console.log("PvE Start Game set up, tear dwon events and start game loop");
+      if (
+        gameBoard.AddShipToGrid(
+          ships[selectedShipIndex],
+          coords,
+          currentDirection
+        )
+      ) {
+        currentLocations.forEach((element) => {
+          element.classList.add("occupied");
+        });
+        selectedShipIndex++;
+        if (selectedShipIndex === ships.length) {
+          console.log("removing click from" + element);
+          //replacing elements removes all listeners
+
+          isPvP
+            ? console.log(
+                "PvP Move onto next player, show blank screen,tear down events and add to other grid"
+              )
+            : startPvEgame(tiles);
+        }
       }
-     }
     });
   });
 }
 
-function hoverPlacementPreview(element,tiles) {
+function hoverPlacementPreview(element, tiles) {
   //remove hover placement from last location
   removeHoverPlacementToBoard();
   currentLocations = [];
@@ -194,59 +151,60 @@ function getVericalPlacementTiles(coords, tiles) {
   return locations;
 }
 
-//choose type of game, PvP or PvE
-
-// [...playerLeftTiles.children].forEach(element => {
-//   element.addEventListener('click',()=>{
-//     console.log(`x: ${element.dataset.col} y: ${element.dataset.row}`);
-//     // element.classList.add("hit");
-//   })
-// });
-
-// [...playerRightTiles.children].forEach(element => {
-//   element.addEventListener('click',()=>{
-//     console.log(`x: ${element.dataset.col} y: ${element.dataset.row}`);
-//     // element.classList.add("miss");
-//   })
-// });
-
-// let computer = new Computer("Computer", gameboardPC);
-// let computer2 = new Computer("Computer2", gameboard);
-// //place random ships on computer grid
-// computer.placeRandomShipsOnBoard();
-// computer2.placeRandomShipsOnBoard();
-
-//simulate game
-
-// while (
-//   computer.gameBoard.AllSunk() !== true &&
-//   computer2.gameBoard.AllSunk() !== true
-// ) {
-
-//   let pcMove = computer2.MakeRandomMove();
-//   let shotTile = playerLeftTiles.querySelector(`[data-row="${pcMove.coords.y}"][data-col="${pcMove.coords.x}"]`)
-//   if (pcMove.status === 1){
-//     shotTile.classList.add("hit");
-//   }else{
-//     shotTile.classList.add("miss");
-//   }
-//   if (computer2.gameBoard.AllSunk()) {
-//      console.log("Computer2 Wins");
-//     break;
-//   }
-
-//    pcMove = computer.MakeRandomMove();
-//    shotTile = playerRightTiles.querySelector(`[data-row="${pcMove.coords.y}"][data-col="${pcMove.coords.x}"]`)
-//   if (pcMove.status === 1){
-//     shotTile.classList.add("hit");
-//   }else{
-//     shotTile.classList.add("miss");
-//   }
-
-//   if (computer.gameBoard.AllSunk()) {
-//      console.log("Computer Wins");
-//     break;
-//   }
-// }
+function startPvEgame(tiles) {
+  [...tiles.children].forEach((x) => {
+    let clone = x.cloneNode(true);
+    x.parentNode.replaceChild(clone, x);
+  });
+  removeHoverFromTiles(playerRightTiles);
+  addHoverToTiles(playerLeftTiles);
+  //simulate Game
 
 
+  let computer = new Computer("Computer", gameboardLeft);
+  computer.placeRandomShipsOnBoard();
+  console.log(computer.gameBoard);
+  let player = new Player("Player", computer.gameBoard);
+  console.log(player);
+  computer = new Computer("Computer", gameboardRight);
+
+  console.log(computer);
+
+  [...playerLeftTiles.children].forEach((element) => {
+    element.addEventListener("click", () => {
+      console.log(player.gameBoard.AllSunk())
+      console.log(computer.gameBoard.AllSunk())
+      if (
+        player.gameBoard.AllSunk() !== true &&
+        computer.gameBoard.AllSunk() !== true
+      ) {
+        let coords = { x: +element.dataset.col, y: +element.dataset.row };
+        let move = player.MakeMove(coords);
+        if (move === 1) {
+          element.classList.add("hit");
+        } else if(move === 0) {
+          element.classList.add("miss");
+        }
+        else{
+          console.log("Bad Move");
+          return
+        }
+        if (player.gameBoard.AllSunk()) {
+          console.log(player.playerName + "Wins");
+        }
+        let pcMove = computer.MakeRandomMove();
+        let shotTile = playerRightTiles.querySelector(`[data-row="${pcMove.coords.y}"][data-col="${pcMove.coords.x}"]`)
+        if (pcMove.status === 1){
+              shotTile.classList.add("hit");
+            }else{
+              shotTile.classList.add("miss");
+            }
+            if (computer.gameBoard.AllSunk()) {
+               console.log(computer.playerName +  "Wins");
+            }
+
+      }
+    });
+  });
+
+}
